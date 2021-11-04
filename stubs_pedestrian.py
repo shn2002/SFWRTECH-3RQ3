a09c10f64e05aaa17b25b39650295890ecc3d1a5
shn2002 <joe.shanhenian@gmail.com>
2021-11-04 17:08:56 -0400

    Add files via upload
    update all

--- stubs_pedestrian.py
+++ stubs_pedestrian.py
@@ -5,12 +5,12 @@
 
 # direction --- has two value WE or NS
 class Pedestrian:
-    def __init__(self, direction ):
-      self.__direction = direction
+    def __init__(self, walk_signal_direction ):
+      self.__walk_signal_direction = walk_signal_direction
 
     # if pedestrian and traffic light are at same dirction and current traffic light is green, pedestrian can cross, otherwise no.
     def is_permitted_for_crossing( self, traffic_light): 
-        if self.direction==traffic_light.direction:
+        if self.walk_signal_direction==traffic_light.direction:
             if traffic_light.color=='GREEN':
                 return True  
         else:
--- test assignment5 part b.py
+++ test assignment5 part b.py
@@ -12,10 +12,10 @@ from stubs_pedestrian import Pedestrian
 # This test covers all the senarios when pedestrian would like to cross street
 # two parameters to determine if pedestrian can pass the street
 #so there are four senarios
-#1.street and pedestrian are at same directions and that traffic light is green
-#2.street and pedestrian are at same directions and that traffic light is red
-#1.street and pedestrian are not at same directions and that traffic light is green
-#1.street and pedestrian are not at same directions and that traffic light is red
+#1.street and the walk signal for pedestrian are at same directions and that traffic light is green
+#2.street and the walk signal for pedestrian are at same directions and that traffic light is red
+#1.street and the walk signal for pedestrian are not at same directions and that traffic light is green
+#1.street and the walk signal for pedestrian are not at same directions and that traffic light is red
 
 def test_pedestrian_cross_street():
     #inital twe traffic lights and one pedestrian
@@ -26,16 +26,16 @@ def test_pedestrian_cross_street():
     main_street_traffic_light.pair(side_street_traffic_light)
     side_street_traffic_light.pair(main_street_traffic_light)
     
-    #1. test if traffic light and pedestrian are same direction but that traffic light is red
+    #1. test if traffic light and the walk signal for pedestrian are same direction but that traffic light is red
     side_street_traffic_light.color='RED'
     assert pedestrian.is_permitted_for_crossing(side_street_traffic_light) == False, "traffic light and pedestrian are at same direction but that traffic light is red can't cross"
-    #2. test if traffic light and pedestrian are same direction and that traffic light is green
+    #2. test if traffic light and the walk signal for pedestrian are same direction and that traffic light is green
     side_street_traffic_light.color='GREEN'
     assert pedestrian.is_permitted_for_crossing(side_street_traffic_light) == True, "traffic light and pedestrian are at same direction, that traffic light is green can cross"
-    #3.test if traffic light and pedestrian are not same direction and that traffic light is red
+    #3.test if traffic light and the walk signal for pedestrian are not same direction and that traffic light is red
     main_street_traffic_light.color='RED'
     assert pedestrian.is_permitted_for_crossing(main_street_traffic_light) == False, "traffic light and pedestrian are not at same direction but can't cross"
-    #3.test if traffic light and pedestrian are not same direction and that traffic light is red
+    #3.test if traffic light and the walk signal for pedestrian are not same direction and that traffic light is red
     main_street_traffic_light.color='GREEN'
     assert pedestrian.is_permitted_for_crossing(main_street_traffic_light) == False, "traffic light and pedestrian are not at same direction even though traffic light is green but can't cross"
     
